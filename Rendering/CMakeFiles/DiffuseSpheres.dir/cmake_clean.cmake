95940147746632b3bcf05641660f5ce00ab69e88
media <zwwang.china@gmail.com>
2016-09-18 22:53:50 -0400

    New CMakeLists for Rendering

---
+++ Rendering/CMakeFiles/DiffuseSpheres.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DiffuseSpheres.dir/DiffuseSpheres.cxx.o"
+  "DiffuseSpheres.pdb"
+  "DiffuseSpheres"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/DiffuseSpheres.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Rendering/CMakeFiles/SpecularSpheres.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpecularSpheres.dir/SpecularSpheres.cxx.o"
+  "SpecularSpheres.pdb"
+  "SpecularSpheres"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SpecularSpheres.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
