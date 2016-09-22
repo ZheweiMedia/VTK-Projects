4e85508f18c4b9c1003aeec10a2c598f52e2cbd2
media <zwwang.china@gmail.com>
2016-09-22 17:32:31 -0400

    For iteration done

---
+++ HW01/CMakeFiles/Cylinder_Series.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Cylinder_Series.dir/Cylinder_Series.cxx.o"
+  "Cylinder_Series.pdb"
+  "Cylinder_Series"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Cylinder_Series.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- HW01/Cylinder_Series.cxx
+++ HW01/Cylinder_Series.cxx
@@ -13,25 +13,46 @@
 
 int main()
 {
-  // Creat a sphere
-  vtkSmartPointer<vtkSphereSource> sphere = vtkSmartPointer<vtkSphereSource>::New();
-  sphere->SetThetaResolution(100);
-  sphere->SetPhiResolution(50);
-
-  vtkSmartPointer<vtkpolyDataMapper> sphereMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
-  sphereMapper->SetInputConnection(sphere->GetOutputPort());
-
   vtkSmartPointer<vtkRenderer> ren = vtkSmartPointer<vtkRenderer>::New();
   vtkSmartPointer<vtkRenderWindow> renWin = vtkSmartPointer<vtkRenderWindow>::New();
   renWin->AddRenderer(ren);
-  vtkSmartPointer<vtkRenderWindowInteractor> iren = vtkSmartPoint<vtkRenderWindowInteractor>::New();
+  vtkSmartPointer<vtkRenderWindowInteractor> iren = vtkSmartPointer<vtkRenderWindowInteractor>::New();
   iren->SetRenderWindow(renWin);
-  // for iteration to get 10 spheres.
-  for(unsighed int i = 0; i < 4; i++)
+
+  // Wait for 100 ms to show next group
+  for (unsigned int j = 0; j < 15; j++)
     {
-      vtkSmartPointer<vtkSphereSource> sphere = vtkSmartPointer<vtkSphereSource>::New();
-      sphere->SetMapper(sphereMapper);
-      sphere->GetProperty()->SetColor(1, 0.1*i,0);
-      sphere->GetProperty()->SetAmbient(0.1*i);
+      // time.sleep(1000);
+      // for iteration to get 4 spheres.
+      for(unsigned int i = 0; i < 4; i++)
+	{
+	  vtkSmartPointer<vtkSphereSource> sphere = vtkSmartPointer<vtkSphereSource>::New();
+	  sphere->SetThetaResolution(100);
+	  sphere->SetPhiResolution(50);
+	  vtkSmartPointer<vtkPolyDataMapper> sphereMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
+	  sphereMapper->SetInputConnection(sphere->GetOutputPort());
+	  vtkSmartPointer<vtkActor> sphere1 = vtkSmartPointer<vtkActor>::New();
+	  sphere1->SetMapper(sphereMapper);
+	  sphere1->GetProperty()->SetColor(1, 0.01*i*j,0);
+	  sphere1->GetProperty()->SetAmbient(0.01*i*j);
+	  sphere1->AddPosition(0.5*i*j, 0, 0);
+
+	  // add actors to the renderer.
+	  ren->AddActor(sphere1);
+	}
+      ren->SetBackground(0.1, 0.2, 0.4);
+      renWin->SetSize(400, 200);
     }
-      
+
+  // set up the lighting.
+  vtkSmartPointer<vtkLight> light = vtkSmartPointer<vtkLight>::New();
+  light->SetFocalPoint(1.875, 0.6125, 0);
+  light->SetPosition(0.875, 1.6125, 1);
+  ren->AddLight(light);
+
+  
+  iren->Initialize();
+  iren->Start();
+
+  return EXIT_SUCCESS;
+}
