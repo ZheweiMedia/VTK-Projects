b457e73abe73b39336cc573794249ffcc8c3f7c2
media <zwwang.china@gmail.com>
2016-09-23 15:36:24 -0400

    Wait for next

--- HW01/Cylinder_Series.cxx
+++ HW01/Cylinder_Series.cxx
@@ -12,38 +12,31 @@
 
 int main()
 {
-  
+  double bottomLine = -100;
  
 
   vtkSmartPointer<vtkRenderWindow> renWin = vtkSmartPointer<vtkRenderWindow>::New();
   vtkSmartPointer<vtkLight> light = vtkSmartPointer<vtkLight>::New();
-  light->SetFocalPoint(1.875, 0.6125, 0);
-  light->SetPosition(0, 0, 10);
+  //light->SetPosition(0, 0, 100);
   vtkSmartPointer<vtkCamera> camera = vtkSmartPointer<vtkCamera>::New();
-  camera->SetPosition(0,0,-50);
+  camera->SetPosition(0,-100,-1000);
   
-
-  
-  
-  int i;
-  int j;
  
-  
-  for (j = 1; j < 50; j++)
+  for (int j = 1; j < 50; j++)
     {
       
       vtkSmartPointer<vtkRenderer> ren = vtkSmartPointer<vtkRenderer>::New();
       
       
       //ren->AddLight(light);
-      //ren->SetActiveCamera(camera);
+      ren->SetActiveCamera(camera);
       for (int k=1; k<20; k++)
 	{
 	  vtkSmartPointer<vtkCylinderSource> cylinder = vtkSmartPointer<vtkCylinderSource>::New();
 	  vtkSmartPointer<vtkPolyDataMapper> cylinderMapper = vtkSmartPointer<vtkPolyDataMapper>::New();
 	  cylinderMapper->SetInputConnection (cylinder->GetOutputPort());
 	  double height = rand()%75;
-	  double radius = 3;
+	  double radius = 5;
 	  cylinder->SetResolution(18);
 	  cylinder->SetHeight(height);
 	  cylinder->SetRadius(radius);
@@ -52,14 +45,14 @@ int main()
 	  cylinderActor->SetMapper(cylinderMapper);
 	  cylinderActor->GetProperty()->SetColor(1.0000, 0.3882, 0.2784);
 	  
-	  cylinderActor->AddPosition(25*k,height/2,0);
+	  cylinderActor->AddPosition(25*k-250,height/2+bottomLine,0);
 	  ren->AddActor( cylinderActor );
 	  ren->SetBackground( 0.1, 0.2, 0.4 );
 	}
       renWin->AddRenderer( ren );
       renWin->SetSize( 600, 300 );
       
-      for (i = 0; i < 10; ++i)
+      for (int i = 0; i < 10; ++i)
 	{
 	  // render the image
 	  renWin->Render();
