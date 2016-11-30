942834b8958fe8e131738f75d5ac0c84a4a9d002
media <zwwang.china@gmail.com>
2016-11-30 11:27:03 -0500

    new demo

--- demo/testCutter.cxx
+++ demo/testCutter.cxx
@@ -8,7 +8,9 @@
  *
  ******************************************************************************/
 
-
+#include <fstream>
+#include <sstream>
+#include <string>
 #include <vtkSmartPointer.h>
 #include <vtkCamera.h>
 #include <vtkNIFTIImageReader.h>
@@ -42,7 +44,8 @@ int main (int argc, char *argv[])
 {
   if (argc < 2)
     {
-      std::cerr << "Required arguments: MRI_file fMRI_file template_file" << std::endl;
+      std::cerr << "Required arguments: MRI_file fMRI_file template_file ROI_index"
+                << std::endl;
       return EXIT_FAILURE;
     }
 
@@ -50,6 +53,7 @@ int main (int argc, char *argv[])
   std::string MRI_name = argv[1];
   std::string fMRI_name = argv[2];
   std::string template_name = argv[3];
+  int ROI_index = atoi(argv[4]);
   double reductionFactor = 1.0;
   double frameRate = 10.0;
   int clip = 0;
@@ -60,11 +64,27 @@ int main (int argc, char *argv[])
   double template_range[2];
 
   int *dim;
+  // initial an array, and set 120 index in it
+  int index[120];
+  int index_lineNo = 0;
 
   // Read file in, and resample, then mapper
 
   vtkAlgorithm *reader;
   vtkImageData *input;
+
+  // read file to get 120 index
+  std::ifstream infile("ROI_index.txt");
+  std::string line;
+  while (std::getline(infile, line))
+    {
+      std::stringstream iss(line);
+      iss >> index[index_lineNo];
+      index_lineNo++;
+    }
+
+  std::cout << index[80] << std::endl;
+
 
   vtkSmartPointer<vtkNIFTIImageReader> template_Reader =
     vtkSmartPointer<vtkNIFTIImageReader>::New();
@@ -74,61 +94,91 @@ int main (int argc, char *argv[])
     vtkSmartPointer<vtkNIFTIImageReader>::New();
   MRI_Reader->SetFileName(MRI_name.c_str());
 
+  vtkSmartPointer<vtkNIFTIImageReader> fMRI_Reader =
+    vtkSmartPointer<vtkNIFTIImageReader>::New();
+  fMRI_Reader->SetFileName(fMRI_name.c_str());
+
   template_Reader->Update();
   MRI_Reader->Update();
+  fMRI_Reader->Update();
 
   vtkSmartPointer<vtkImageResample> resample =
     vtkSmartPointer<vtkImageResample>::New();
 
-  vtkSmartPointer<vtkFixedPointVolumeRayCastMapper> mapper =
+  vtkSmartPointer<vtkFixedPointVolumeRayCastMapper> MRI_mapper =
     vtkSmartPointer<vtkFixedPointVolumeRayCastMapper>::New();
   //mapper->SetInputConnection(reader->GetOutputPort());
+  vtkSmartPointer<vtkFixedPointVolumeRayCastMapper> fMRI_mapper =
+    vtkSmartPointer<vtkFixedPointVolumeRayCastMapper>::New();
+  vtkSmartPointer<vtkFixedPointVolumeRayCastMapper> template_mapper =
+    vtkSmartPointer<vtkFixedPointVolumeRayCastMapper>::New();
 
   // deep copy data to imagedata
 
   vtkSmartPointer<vtkImageData> MRI_image =
     vtkSmartPointer<vtkImageData>::New();
   MRI_image->ShallowCopy(MRI_Reader->GetOutput());
-  int* dims = MRI_image->GetDimensions();
-  std::cout << dims[0] << " " << dims[1] << " " << dims[2] << std::endl;
+
+  vtkSmartPointer<vtkImageData> fMRI_image =
+    vtkSmartPointer<vtkImageData>::New();
+  fMRI_image->ShallowCopy(template_Reader->GetOutput());
   
-
-
   vtkSmartPointer<vtkImageData> template_image =
     vtkSmartPointer<vtkImageData>::New();
   template_image->ShallowCopy(template_Reader->GetOutput());
+
+  int* dims = MRI_image->GetDimensions();
+  std::cout << dims[0] << " " << dims[1] << " " << dims[2] << std::endl;
   
   std::cout << MRI_image->GetScalarTypeAsString() << std::endl;
+  std::cout << fMRI_image->GetScalarTypeAsString() << std::endl;
   std::cout << template_image->GetScalarTypeAsString() << std::endl;
+  
 
   for (int i = 0; i < dims[0]; i++)
     for (int j = 0; j < dims[1]; j++)
       for (int k = 0; k < dims[2]; k++)
         {
-          short* template_pixel =
-            static_cast<short*>(template_image->GetScalarPointer(i, j, k));
           float* MRI_pixel =
             static_cast<float*>(MRI_image->GetScalarPointer(i, j, k));
-          if (template_pixel[0] == 4101.0)
+          short* template_pixel =
+            static_cast<short*>(template_image->GetScalarPointer(i, j, k));
+          short* fMRI_pixel =
+            static_cast<short*>(fMRI_image->GetScalarPointer(i, j, k));
+
+          if (template_pixel[0] == index[ROI_index])
             {
               template_pixel[0] = 255.0;
             }
           else
             {
               template_pixel[0] = 0;
-              MRI_pixel[0] = MRI_pixel[0]/10;
+              MRI_pixel[0] = MRI_pixel[0]/8;
+              fMRI_pixel[0] = fMRI_pixel[0]/8;
             }
             
         }
 
   MRI_image->GetScalarRange(MRI_range);
+  fMRI_image->GetScalarRange(fMRI_range);
   std::cout << MRI_range[0] << " " << MRI_range[1] << std::endl;
 
-  mapper->SetInputData(MRI_image);
-  mapper->SetBlendModeToMaximumIntensity();
+  MRI_mapper->SetInputData(MRI_image);
+  MRI_mapper->SetBlendModeToMaximumIntensity();
+
+  fMRI_mapper->SetInputData(MRI_image);
+  fMRI_mapper->SetBlendModeToMaximumIntensity();
 
   
   // Create a transfer function mapping scalar value to opacity and color
+
+  vtkSmartPointer<vtkVolumeProperty> MRI_Property =
+    property(MRI_range[0], MRI_range[1], independentComponents, 'G');
+
+  vtkSmartPointer<vtkVolumeProperty> fMRI_Property =
+    property(fMRI_range[0], fMRI_range[1], independentComponents, 'R');
+
+
 
   vtkSmartPointer<vtkVolumeProperty> template_Property =
     property(template_range[0], template_range[1], independentComponents, 'B');
@@ -136,10 +186,17 @@ int main (int argc, char *argv[])
 
   
 
+  vtkSmartPointer<vtkVolume> MRI_volume =
+    vtkSmartPointer<vtkVolume>::New();
+  vtkSmartPointer<vtkVolume> fMRI_volume =
+    vtkSmartPointer<vtkVolume>::New();
   vtkSmartPointer<vtkVolume> template_volume =
     vtkSmartPointer<vtkVolume>::New();
-  template_volume->SetMapper(mapper);
-  template_volume->SetProperty(template_Property);
+  MRI_volume->SetMapper(MRI_mapper);
+  MRI_volume->SetProperty(MRI_Property);
+
+  fMRI_volume->SetMapper(fMRI_mapper);
+  fMRI_volume->SetProperty(fMRI_Property);
 
   // Create the renderers, render window, and interactor
 
@@ -157,9 +214,9 @@ int main (int argc, char *argv[])
   
   renWin->SetSize(600, 600);
   renWin->Render();
-  //ren->AddVolume(MRI_volume);
-  //ren->AddVolume(fMRI_volume);
-  ren->AddVolume(template_volume);
+  ren->AddVolume(MRI_volume);
+  ren->AddVolume(fMRI_volume);
+  //ren->AddVolume(template_volume);
   ren->ResetCamera();
   renWin->Render();
   iren->Start();
