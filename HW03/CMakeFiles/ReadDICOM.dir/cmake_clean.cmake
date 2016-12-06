773af3f9a219d1bc9951b8383419f05415295d03
Zhewei <zwwang.china@gmail.com>
2016-12-06 00:47:33 -0500

    demo

---
+++ HW03/CMakeFiles/ReadDICOM.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ReadDICOM.dir/ReadDICOM.cxx.o"
+  "ReadDICOM.pdb"
+  "ReadDICOM"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ReadDICOM.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
